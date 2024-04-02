f87a8317716b76e8e3a53430d47e3d16770abefa
Matias <mcornalo@frba.utn.edu.ar>
2024-04-02 14:28:50 +0000

    Agrego funciones y un menu provisorio

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -3,6 +3,45 @@
 #include <utils/hello.h>
 
 int main(int argc, char* argv[]) {
-    decir_hola("Kernel");
+    int respuesta;
+    do {
+        printf("---------------Menu---------------");
+        printf("Elija una opcion (numero)");
+        printf("---------------Menu---------------");
+        printf("---------------Menu---------------");
+        printf("---------------Menu---------------");
+        scanf("%d",respuesta);
+        
+    } while(respuesta != 5);
     return 0;
+}
+
+void EJECUTAR_SCRIPT(/*path*/)
+{
+
+}
+
+void INICIAR_PROCESO(/*path*/)
+{
+
+}
+
+void FINALIZAR_PROCESO(/*pid*/)
+{
+
+}
+
+void DETENER_PLANIFICACION()
+{
+
+}
+
+void INICIAR_PLANIFICACION()
+{
+
+}
+
+void PROCESO_ESTADO()
+{
+
 }
