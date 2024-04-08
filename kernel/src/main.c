8ee2f8df370f99b3443847e067ccb07c2a0ba3bf
Matias <mcornalo@frba.utn.edu.ar>
2024-04-08 12:08:02 +0000

    Agrego comentarios utiles y la consola interactiva

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -5,17 +5,58 @@
 int main(int argc, char* argv[]) {
     int respuesta;
     do {
-        printf("---------------Menu---------------");
-        printf("Elija una opcion (numero)");
-        printf("---------------Menu---------------");
-        printf("---------------Menu---------------");
-        printf("---------------Menu---------------");
+        printf("---------------Consola interactiva Kernel---------------\n");
+        printf("Elija una opcion (numero)\n");
+        printf("1- Ejecutar Script \n");
+        printf("2- Iniciar proceso\n");
+        printf("3- Finalizar proceso\n");
+        printf("4- Iniciar planificacion\n");
+        printf("5- Detener planificacion\n");
+        printf("6- Listar procesos por estado\n");
+        printf("7- Finalizar modulo");
         scanf("%d",respuesta);
+        switch (respuesta)
+        {
+        case 1:
+            EJECUTAR_SCRIPT(/* pathing del conjunto de instrucciones*/);
+            break;
         
-    } while(respuesta != 5);
+        case 2:
+            INICIAR_PROCESO(/*Recibe el pathing*/);
+            break;
+        
+        case 3:
+            FINALIZAR_PROCESO(/*pid*/);
+            break;
+        
+        case 4:
+            INICIAR_PLANIFICACION(); // Esto depende del algoritmo vigente (FIFO, RR,VRR), se deben poder cambiar
+            break;
+        
+        case 5:
+            DETENER_PLANIFICACION(); // Ver linea 33
+            break;
+        
+        case 6:
+            PROCESO_ESTADO();
+            break;
+        
+        case 7:
+            printf("Finalizacion del modulo");
+            exit(1); 
+            break;
+        
+        default:
+            printf("No se encuentra el numero dentro de las opciones, porfavor elija una correcta");
+            break;
+        }
+        
+    } while(respuesta != 7); // Modificar respuesta si se agregan mas elementos en la consola
     return 0;
 }
 
+// La idea es que el usuario (nosotros) no estemos todo el dia con la consola interactiva
+// Por eso tenemos el ejecutar escript, que recibe una lista de instrucciones que debe realizar en seguidilla este modulo
 void EJECUTAR_SCRIPT(/*path*/)
 {
 
@@ -41,7 +82,7 @@ void INICIAR_PLANIFICACION()
 
 }
 
-void PROCESO_ESTADO()
+void PROCESO_ESTADO() // Esta funcion es para listar los procesos estado
 {
 
 }
